4dc00e6144f735a9ee5a8d66edd46c9f1931e131
bbbbruno <bbbbruno69@gmail.com>
2022-01-06 15:00:50 +0900

    synonymsコマンドを作成

---
+++ domain-search/synonyms/go.mod
@@ -0,0 +1,9 @@
+module synonyms
+
+go 1.17
+
+require thesaurus/thesaurus v0.0.0
+
+require github.com/joho/godotenv v1.4.0
+
+replace thesaurus/thesaurus => ../thesaurus
---
+++ domain-search/synonyms/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"thesaurus/thesaurus"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	apiKey := os.Getenv("BHT_APIKEY")
+	thesaurus := &thesaurus.BigHuge{APIKey: apiKey}
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		word := s.Text()
+		syns, err := thesaurus.Synonyms(word)
+		if err != nil {
+			log.Fatalf("%qの類語検索に失敗しました： %v\n", word, err)
+		}
+		if len(syns) == 0 {
+			log.Fatalf("%qに類語はありませんでした\n", word)
+		}
+		for _, syn := range syns {
+			fmt.Println(syn)
+		}
+	}
+}
