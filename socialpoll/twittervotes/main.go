5cab3807c96a0ee22f4afc94005831bd21ef69c7
bbbbruno <bbbbruno69@gmail.com>
2022-01-18 21:02:54 +0900

    twitter.goをリファクタリング

--- socialpoll/twittervotes/main.go
+++ socialpoll/twittervotes/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"context"
 	"io"
 	"log"
 	"net"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 	"time"
 
@@ -70,37 +70,25 @@ func loadOptions() ([]string, error) {
 	return options, iter.Err()
 }
 
-func publishVotes(votes <-chan string) <-chan struct{} {
-	stopchan := make(chan struct{}, 1)
+func publishVotes(votes <-chan string) {
 	pub, _ := nsq.NewProducer("localhost:4150", nsq.NewConfig())
-	go func() {
-		for vote := range votes {
-			pub.Publish("votes", []byte(vote))
-		}
-		log.Println("Pubisher: 停止中です")
-		pub.Stop()
-		log.Println("Publisher: 停止しました")
-		stopchan <- struct{}{}
-	}()
-	return stopchan
+	for vote := range votes {
+		pub.Publish("votes", []byte(vote))
+	}
+	log.Println("Pubisher: 停止中です")
+	pub.Stop()
+	log.Println("Publisher: 停止しました")
 }
 
 func main() {
-	var stoplock sync.Mutex
-	stop := false
-	stopChan := make(chan struct{}, 1)
+	ctx, cancel := context.WithCancel(context.Background())
 	signalChan := make(chan os.Signal, 1)
 
 	go func() {
 		<-signalChan
-		stoplock.Lock()
-		stop = true
-		stoplock.Unlock()
+		cancel()
 		log.Println("停止します...")
-		stopChan <- struct{}{}
-		closeConn()
 	}()
-
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
 	if err := dialdb(); err != nil {
@@ -109,21 +97,6 @@ func main() {
 	defer closedb()
 
 	votes := make(chan string)
-	publisherStoppedChan := publishVotes(votes)
-	twitterStoppedChan := startTwitterStream(stopChan, votes)
-	go func() {
-		for {
-			time.Sleep(1 * time.Minute)
-			closeConn()
-			stoplock.Lock()
-			if stop {
-				stoplock.Unlock()
-				break
-			}
-			stoplock.Unlock()
-		}
-	}()
-	<-twitterStoppedChan
-	close(votes)
-	<-publisherStoppedChan
+	go twitterStream(ctx, votes)
+	publishVotes(votes)
 }
